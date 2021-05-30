1622aa1286328b896daa7e360a0f1f005e04fa5e
Artiphues <nise44@live.com>
2021-05-30 18:45:56 +0400

    Update

--- functions.js
+++ functions.js
@@ -5,11 +5,12 @@ if(typeof linksAndTitles == 'undefined')
 {
 	linksAndTitles = "";
 }
-for(let i = 0; i < document.getElementsByClassName("yt-simple-endpoint style-scope ytd-playlist-video-renderer").length; i = i + 1)
+for(let i = 0; i < document.getElementsByClassName("style-scope ytd-playlist-video-list-renderer")[3].children.length; i = i + 1)
 {
-	let current = document.getElementsByClassName("yt-simple-endpoint style-scope ytd-playlist-video-renderer")[i];
-	let title = current.children[1].children[0].children[1].innerText;
-	let combined = current.href + " - " + title;
+	let current = document.getElementsByClassName("style-scope ytd-playlist-video-list-renderer")[3].children[i];
+	let title = current.children[1].children[0].children[1].children[0].innerText;
+	let destination = current.children[1].children[0].children[1].children[0].children[1].href;
+	let combined = destination + " - " + title;
 	linksAndTitles += combined + "\n";
 }
 chrome.runtime.sendMessage({links: linksAndTitles}, function(response) {
